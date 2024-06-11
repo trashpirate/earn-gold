886f1d66f593e75544ba6faffa1dc4872f1b497a
trashpirate <nadinaoates@gmail.com>
2024-06-11 11:14:29 -0500

    build out deposit function

--- src/EGCEngine.sol
+++ src/EGCEngine.sol
@@ -1,7 +1,9 @@
 // SPDX-License Identifier: MIT
-
 pragma solidity 0.8.20;
 
+import {EarnGold} from "./EarnGold.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 /**
  * @title EarnGold
  * @author Nadina Oates
@@ -17,9 +19,88 @@ pragma solidity 0.8.20;
  * @notice This contract is VERY loolsely base on the MakerDAO DSS (DAI) system.
  */
 
-contract EGCEngine {
+contract EGCEngine is ReentrancyGuard {
+    /**
+     * State variables
+     */
+    mapping(address token => address priceFeed) private s_priceFeeds;
+    mapping(address user => mapping(address token => uint256 amount)) private s_collateralDeposited;
+
+    EarnGold private immutable i_egc;
+
+    /**
+     * Events
+     */
+    event CollateralDeposited(address indexed user, address indexed token, uint256 indexed amount);
+
+    /**
+     * Errors
+     */
+    error EGCEngine__MustBeMoreThanZero();
+    error EGCEngine__UnequalNumberOfTokenAndPriceFeedAddresses();
+    error EGCEngine__TokenNotAllowed();
+    error EGCEngine__TransferFailed();
+
+    /**
+     * Modifiers
+     */
+    modifier moreThanZero(uint256 amount) {
+        if (amount == 0) {
+            revert EGCEngine__MustBeMoreThanZero();
+        }
+        _;
+    }
+
+    modifier isAllowedToken(address token) {
+        if (s_priceFeeds[token] == address(0)) {
+            revert EGCEngine__TokenNotAllowed();
+        }
+        _;
+    }
+
+    /**
+     * Functions
+     */
+    constructor(address[] memory tokenAddresses, address[] memory priceFeedAddresses, address egcAddress) {
+        // USD Price Feeds
+        if (tokenAddresses.length != priceFeedAddresses.length) {
+            revert EGCEngine__UnequalNumberOfTokenAndPriceFeedAddresses();
+        }
+
+        for (uint256 i = 0; i < tokenAddresses.length; i++) {
+            s_priceFeeds[tokenAddresses[i]] = priceFeedAddresses[i];
+        }
+
+        i_egc = EarnGold(egcAddress);
+    }
+
+    /**
+     * External Functions
+     */
     function depositCollateralAndMintEGC() external {}
-    function depositCollateral() external {}
+
+    /**
+     * @notice follows CEI
+     * @param tokenCollateralAddress The address fo the token to deposit as collateral
+     * @param amountCollateral The amount of collateral to deposit
+     */
+    function depositCollateral(address tokenCollateralAddress, uint256 amountCollateral)
+        external
+        isAllowedToken(tokenCollateralAddress)
+        moreThanZero(amountCollateral)
+        nonReentrant
+    {
+        s_collateralDeposited[msg.sender][tokenCollateralAddress] = amountCollateral;
+        emit CollateralDeposited(msg.sender, tokenCollateralAddress, amountCollateral);
+
+        bool success = IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this), amountCollateral);
+        if (!success) {
+            revert EGCEngine__TransferFailed();
+        }
+    }
+
+    /**
+     */
     function redeemCollateralForDSC() external {}
     function redeemCollateral() external {}
     function mintDSC() external {}
